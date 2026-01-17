2638abd457e25e1dbeba7b25a45e33241beb33a9
André Vitor da Silva Cardoso <98904347+AndreAnimator@users.noreply.github.com>
2026-01-17 18:47:05 -0300

    Added current astro's way of content handling

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,8 @@
 import { defineCollection, z } from "astro:content";
+import { file, glob } from "astro/loaders";
 
 const postsCollection = defineCollection({
+    loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/posts" }),
     schema: ({ image }) => z.object({
         author: z.string(),
         date: z.string(),
